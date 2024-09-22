19d4146e5ca7602b2c39f0e2c9b3f7c4c193c27e
Matheus <matheus.sam.10@gmail.com>
2024-09-22 13:22:34 -0300

    Create Todo class on model

--- ToDoList/ToDoApp/models.py
+++ ToDoList/ToDoApp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class ToDo(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=180)
+    details = models.CharField(max_length=300)
+    is_checked = models.BooleanField()
+    deadline = models.DateField()
+
+    def __str__(self):
+        return self.name
+
+# TO-DO: add class Category(models.Model)
+# To-DO: add class User(models.Model)

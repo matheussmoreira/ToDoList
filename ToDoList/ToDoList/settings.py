ada078073fa5b0d7740f129c992c1a20a8e9b07b
victorletichevsky <victorletichevsky@gmail.com>
2024-10-08 11:55:13 -0300

    Adicionando chave para permitir executar na aws, esse arquivo com a chave nao vai pro git

--- ToDoList/ToDoList/settings.py
+++ ToDoList/ToDoList/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import allowed_host
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-b8ug=ha*odf2a9y^%@6_t(k+ylywnqu*2$s2vnq5fx_x%*+1du
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [allowed_host.SECRET_HOST]
 
 
 # Application definition
